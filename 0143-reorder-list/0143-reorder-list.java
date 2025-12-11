5b46677cfb833ba6761695cfc9d68288b837b753
Mkatpelly <107731228+Mkatpelly@users.noreply.github.com>
2025-12-11 13:35:52 -0800

    Time: 2 ms (89.13%), Space: 49.2 MB (32.2%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) {
+            return;
+        }
+        ListNode middle = findMiddle(head);
+        ListNode secondHalf = middle.next;
+        middle.next = null;
+        secondHalf = reverseList(secondHalf);
+        mergeLists(head, secondHalf);
+    }
+    private ListNode findMiddle(ListNode head) {
+        ListNode slow = head, fast = head;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return slow;
+    }
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null, curr = head;
+        
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        
+        return prev;
+    }
+    private void mergeLists(ListNode l1, ListNode l2) {
+        ListNode p1 = l1, p2 = l2;
+        
+        while (p2 != null) {
+            ListNode temp1 = p1.next;
+            ListNode temp2 = p2.next;
+            p1.next = p2;
+            p2.next = temp1;
+            p1 = temp1;
+            p2 = temp2;
+        }
+    }
+}
