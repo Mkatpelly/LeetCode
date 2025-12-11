0ab2b97929cd299eed2edad78b7ec797c137fcbf
Mkatpelly <107731228+Mkatpelly@users.noreply.github.com>
2025-12-11 15:39:10 -0800

    Time: 1 ms (99.74%), Space: 47.7 MB (33.26%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int nonZeroIndex = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                nums[nonZeroIndex++] = nums[i];
+            }
+        }
+        for (int i = nonZeroIndex; i < n; i++) {
+            nums[i] = 0;
+        }
+    }
+}
