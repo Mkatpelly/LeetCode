d3caf3da41c95abdb2b634c8c8ccd2cb096576ea
Mkatpelly <107731228+Mkatpelly@users.noreply.github.com>
2025-12-11 16:45:46 -0800

    Time: 0 ms (100%), Space: 43.9 MB (9.13%) - LeetHub

---
+++ 0426-convert-binary-search-tree-to-sorted-doubly-linked-list/0426-convert-binary-search-tree-to-sorted-doubly-linked-list.java
@@ -0,0 +1,49 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val,Node _left,Node _right) {
+        val = _val;
+        left = _left;
+        right = _right;
+    }
+};
+*/
+
+class Solution {
+    private Node first = null;
+    private Node last = null;
+    public Node treeToDoublyList(Node root) {
+        if (root == null) {
+            return null;
+        }
+        inOrderTraversal(root);
+        last.right = first;
+        first.left = last;
+        return first;
+    }
+    private void inOrderTraversal(Node node) {
+        if (node == null) {
+            return;
+        }
+        inOrderTraversal(node.left);
+        if (last != null) {
+            last.right = node;
+            node.left = last;
+        } else {
+            first = node;
+        }
+        last = node;
+        inOrderTraversal(node.right);
+    }
+
+}
