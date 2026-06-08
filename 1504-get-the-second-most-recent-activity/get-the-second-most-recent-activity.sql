491c8053ee2525d2e925159e8ee254fa90bf2186
Munvith Katpelly <107731228+Mkatpelly@users.noreply.github.com>
2026-06-07 20:32:14 -0700

    Time: 386 ms (57.69%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1504-get-the-second-most-recent-activity/get-the-second-most-recent-activity.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select username, activity, startDate, endDate
+from (
+select *, count(activity) over(partition by username)cnt, 
+ROW_NUMBER() over(partition by username order by startdate desc) n from UserActivity) tbl
+where n=2 or cnt<2
