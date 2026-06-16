101b8e1de91c6a26e594643f7195234cbfc78615
Munvith Katpelly <107731228+Mkatpelly@users.noreply.github.com>
2026-06-16 16:25:55 -0700

    Time: 535 ms (19.15%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2208-account-balance/account-balance.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select t1.account_id ,t1.day ,sum(case when t2.type='Deposit' then t2.amount else -1*t2.amount end) as balance 
+from transactions t1 ,transactions t2 
+where t1.account_id =t2.account_id and t1.day>=t2.day 
+group by t1.account_id ,t1.day 
+order by account_id asc ,day asc
