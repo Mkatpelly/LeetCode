6563602cb3b4232d0d4a3c23552eda64712e235a
Munvith Katpelly <107731228+Mkatpelly@users.noreply.github.com>
2026-06-16 10:35:09 -0700

    Time: 2730 ms (12.16%) | Memory: 0B (100.00%) - LeetSync

--- 1688-the-most-recent-orders-for-each-product/the-most-recent-orders-for-each-product.sql
+++ 1688-the-most-recent-orders-for-each-product/the-most-recent-orders-for-each-product.sql
@@ -1,18 +1,9 @@
 # Write your MySQL query statement below
-
-select
-    product_name, product_id, order_id, order_date
-from
-    Orders
-    join
-    Products
-    using (product_id)
-where (product_id, order_date) in
-    (select
-        product_id, max(order_date) as order_date
-    from
-        Orders
-    group by
-        product_id)
-order by
-    product_name, product_id, order_id
+select b.product_name, a.product_id, a.order_id, a.order_date from Orders a
+join Products b
+on a.product_id = b.product_id
+where (a.product_id, a.order_date) in (
+select product_id, max(order_date) as order_date
+from Orders
+group by product_id)
+order by b.product_name, a.product_id, a.order_id
