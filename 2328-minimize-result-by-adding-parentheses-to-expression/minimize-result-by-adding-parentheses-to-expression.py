b25c7ea8af7cab321861c085c6ca126dbbc995da
Munvith Katpelly <107731228+Mkatpelly@users.noreply.github.com>
2026-08-17 13:47:12 -0700

    Time: 7 ms (12.06%) | Memory: 19.1 MB (92.49%) - LeetSync

---
+++ 2328-minimize-result-by-adding-parentheses-to-expression/minimize-result-by-adding-parentheses-to-expression.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimizeResult(self, expression: str) -> str:
+        left, right = expression.split('+')                             
+        value = lambda s:eval(s.replace('(','*(').replace(')',')*').strip('*'))    
+                                                                           
+        lft = [ left[0:i]+'('+ left[i:] for i in range(  len(left )  )] 
+        rgt = [right[0:i]+')'+right[i:] for i in range(1,len(right)+1)] 
+        return  min([l+'+'+r for l in lft for r in rgt], key = value)
