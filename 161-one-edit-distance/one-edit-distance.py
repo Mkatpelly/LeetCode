2cf1e3fb330d6d7a99303d8cf22a9b3d3d97a98f
Munvith Katpelly <107731228+Mkatpelly@users.noreply.github.com>
2026-08-21 12:41:59 -0700

    Time: 0 ms (100.00%) | Memory: 19.4 MB (32.42%) - LeetSync

--- 161-one-edit-distance/one-edit-distance.py
+++ 161-one-edit-distance/one-edit-distance.py
@@ -1,18 +1,7 @@
-class Solution(object):
-    def isOneEditDistance(self, s, t):
-        if s == t:
-            return False
-        l1, l2 = len(s), len(t)
-        if l1 > l2: # force s no longer than t
-            return self.isOneEditDistance(t, s)
-        if l2 - l1 > 1:
-            return False
-        for i in xrange(len(s)):
-            if s[i] != t[i]:
-                if l1 == l2:
-                    s = s[:i]+t[i]+s[i+1:]  # replacement
-                else:
-                    s = s[:i]+t[i]+s[i:]  # insertion
-                break
-        return s == t or s == t[:-1]
-            
+class Solution:
+    def isOneEditDistance(self, s: str, t: str) -> bool:
+        if s == t: return False
+        i = 0
+        while i < min(len(s), len(t)) and s[i] == t[i]:
+            i += 1
+        return s[i:] == t[i+1:] or s[i+1:] == t[i:] or s[i+1:] == t[i+1:]
