d9cf1e6e10fc283789dba28498bdc41e4e74fd04
Munvith Katpelly <107731228+Mkatpelly@users.noreply.github.com>
2026-09-05 13:10:42 -0700

    Time: 149 ms (52.10%) | Memory: 79 MB (43.60%) - LeetSync

--- 460-lfu-cache/lfu-cache.py
+++ 460-lfu-cache/lfu-cache.py
@@ -1,119 +1,75 @@
-class Node(object):
-    __slots__ = ("key", "val", "freq", "prev", "next")
-    def __init__(self, key, val, freq=1):
+class ListNode:
+    def __init__(self, key, val):
         self.key = key
         self.val = val
-        self.freq = freq
+        self.freq = 1
         self.prev = None
         self.next = None
-
-
-class DoublyLinkedList(object):
+        
+class DLL:
     def __init__(self):
-        self.head = Node(None, None)  # dummy head
-        self.tail = Node(None, None)  # dummy tail
+        self.head = ListNode(0, 0)
+        self.tail = ListNode(0, 0)
         self.head.next = self.tail
         self.tail.prev = self.head
         self.size = 0
-
-    def appendleft(self, node):
-        # insert node right after head (most recent)
-        node.next = self.head.next
-        node.prev = self.head
-        self.head.next.prev = node
+        
+    def insertHead(self, node):
+        headNext = self.head.next
+        headNext.prev = node
         self.head.next = node
+        node.prev = self.head
+        node.next = headNext
         self.size += 1
-
-    def remove(self, node):
-        # remove an existing node
-        prev = node.prev
-        nxt = node.next
-        prev.next = nxt
-        nxt.prev = prev
+        
+    def removeNode(self, node):
+        node.next.prev = node.prev
+        node.prev.next = node.next
         self.size -= 1
+        
+    def removeTail(self):
+        tail = self.tail.prev
+        self.removeNode(tail)
+        return tail
+    
 
-    def pop(self):
-        # remove and return LRU node (before tail)
-        if self.size == 0:
-            return None
-        node = self.tail.prev
-        self.remove(node)
-        return node
+class LFUCache:
 
-
-class LFUCache(object):
-
-    def __init__(self, capacity):
-        """
-        :type capacity: int
-        """
+    def __init__(self, capacity: int):
+        self.cache = {}
+        self.freqTable = collections.defaultdict(DLL)
         self.capacity = capacity
-        self.size = 0
         self.minFreq = 0
-        self.keyToNode = {}      # key -> Node
-        self.freqToList = {}     # freq -> DoublyLinkedList
-
-    def _update_freq(self, node):
-        """Move node from freq f to f+1 list."""
-        f = node.freq
-        lst = self.freqToList[f]
-        lst.remove(node)
-
-        # If this was the only node with minFreq, increase minFreq
-        if f == self.minFreq and lst.size == 0:
-            self.minFreq += 1
-
-        node.freq += 1
-        f2 = node.freq
-        if f2 not in self.freqToList:
-            self.freqToList[f2] = DoublyLinkedList()
-        self.freqToList[f2].appendleft(node)
-
-    def get(self, key):
-        """
-        :type key: int
-        :rtype: int
-        """
-        if key not in self.keyToNode:
+        
+    
+    def get(self, key: int) -> int:
+        if key not in self.cache:
             return -1
-        node = self.keyToNode[key]
-        self._update_freq(node)
+        return self.updateCache(self.cache[key], key, self.cache[key].val)
+        
+
+    def put(self, key: int, value: int) -> None:
+        if not self.capacity:
+            return
+        if key in self.cache:
+            self.updateCache(self.cache[key], key, value)
+        else:
+            if len(self.cache) == self.capacity:
+                prevTail = self.freqTable[self.minFreq].removeTail()
+                del self.cache[prevTail.key]
+            node = ListNode(key, value)
+            self.freqTable[1].insertHead(node)
+            self.cache[key] = node
+            self.minFreq = 1
+        
+    
+    def updateCache(self, node, key, value):
+        node = self.cache[key]
+        node.val = value
+        prevFreq = node.freq
+        node.freq += 1
+        self.freqTable[prevFreq].removeNode(node)
+        self.freqTable[node.freq].insertHead(node)
+        if prevFreq == self.minFreq and self.freqTable[prevFreq].size == 0:
+            self.minFreq += 1
         return node.val
-
-    def put(self, key, value):
-        """
-        :type key: int
-        :type value: int
-        :rtype: None
-        """
-        if self.capacity == 0:
-            return
-
-        if key in self.keyToNode:
-            node = self.keyToNode[key]
-            node.val = value
-            self._update_freq(node)
-            return
-
-        # Need to insert a new key
-        if self.size == self.capacity:
-            # evict LFU (and LRU among them)
-            lst = self.freqToList[self.minFreq]
-            node_to_remove = lst.pop()
-            del self.keyToNode[node_to_remove.key]
-            self.size -= 1
-
-        # insert new node with freq 1
-        new_node = Node(key, value, freq=1)
-        self.keyToNode[key] = new_node
-        if 1 not in self.freqToList:
-            self.freqToList[1] = DoublyLinkedList()
-        self.freqToList[1].appendleft(new_node)
-        self.minFreq = 1
-        self.size += 1
-
-
-# Your LFUCache object will be instantiated and called as such:
-# obj = LFUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
